071f70d976cc7c9fe862e9565968d149a5f35702
Matthieu Rode <matthieurode.web@gmail.com>
2021-02-07 17:47:53 +0100

    gitflow-feature-stash: some_ux_changes

--- edco.py
+++ edco.py
@@ -12,73 +12,68 @@ standings = ""
 path = ""
 
 def main():
-    parser = argparse.ArgumentParser(description="Pour utiliser l'outil, tu dois remplir les arguments ci dessous : ")
+    #parser = argparse.ArgumentParser(description="Pour utiliser l'outil, tu dois remplir les arguments ci dessous : ")
 
-    parser.add_argument('--mode', "-M", help='GOD, OG, AG')
-    parser.add_argument('--gameId', "-ID", help='Optionnel - L\'id du match pour le Game Of The Day')
-    parser.add_argument('--date', "-D", help='Optionnel - Tu dois remplir la date voulue sur la forme YYYYMMDD (ex : 20210521) - Defaut : today')
+    #parser.add_argument('--mode', "-M", help='GOD, OG, AG')
+    #parser.add_argument('--gameId', "-ID", help='Optionnel - L\'id du match pour le Game Of The Day')
+    #parser.add_argument('--date', "-D", help='Optionnel - Tu dois remplir la date voulue sur la forme YYYYMMDD (ex : 20210521) - Defaut : today')
 
-    args = parser.parse_args()
-    if args.date and len(args.date) == 8:
-        global today
-        today = args.date
+    #args = parser.parse_args()
+    #if args.date and len(args.date) == 8:
+        #global today
+        #today = args.date
 
     global scoreboard
     global standings
+
     standings = requests.get("http://data.nba.net/prod/v1/current/standings_all.json").json()
     scoreboard = requests.get("http://data.nba.net/10s/prod/v1/"+today+"/scoreboard.json").json()
 
     
     getAllGames()
+    getOtherGames()
 
-    if(args.gameId):
-        getGameOfTheDay(args.gameId)
-    else:
-        parseMode(args.mode)
+    #if(args.gameId):
+        #getGameOfTheDay(args.gameId)
+    #else:
+        #parseMode(args.mode)
 
-def parseMode(mode):
-    print("Selected mode : " + mode)
-    if mode == "OG":
-        getOtherGames()
-    if mode == "AG":
-        getAllGames()
+
 
 def getAllGames():
-    
-    #print("Il y a " + scoreboard["numGames"].text + " matches")
     global scoreboard
+    print("Il y a " + str(scoreboard["numGames"]) + " matches : ")
+    
     for match in scoreboard["games"]:
         print(match["gameId"] + " " + match["hTeam"]["triCode"] + " vs " + match["vTeam"]["triCode"])
-
-
-def getTeamStats(teamId):
-    for team in standings["league"]["standard"]["teams"]:
-        if(team["teamId"] == teamId):
-            print(team["confRank"])
-            print(team["homeWin"] + "-" + team["homeLoss"])
-            print(team["awayWin"] + "-" + team["awayLoss"])
+    
+    print("")
 
 def getOtherGames():
-    ids = input("Entrer les codes des matchs de l'affiche principales et des affiches importantes :")
+    ids = input("Entrer les codes des matchs de l'affiche principales et des affiches importantes avec un espace entre chaque code : ")
     codes = ids.split()
 
     global scoreboard
+
     for match in scoreboard["games"]:
         if match["gameId"] in codes:
             scoreboard["games"].remove(match)
     
     path = os.path.join(pathlib.Path().absolute(), "templates", "other_games", getPhotoshopFile(len(scoreboard["games"])))
-    #path = os.path.join(pathlib.Path().absolute(), "templates", "other_games", getPhotoshopFile(1))
-
     doPhotoshopStuff(path)
 
 def doPhotoshopStuff(path):
+    print("Ouveture de Photoshop")
     psApp = win32com.client.Dispatch("Photoshop.Application")
     psApp.Open(path)
     doc = psApp.Application.ActiveDocument
+    print("Photoshop est ouvert")
+    print("")
 
     global scoreboard
     global standings
+
+    teams = standings["league"]["standard"]["teams"]
 
     for match in scoreboard["games"]:
         index = scoreboard["games"].index(match) + 1
@@ -87,35 +82,23 @@ def doPhotoshopStuff(path):
         vd_domicile = doc.ArtLayers["hteam_vd_game_" + str(index)]
         vd_exterieur = doc.ArtLayers["vteam_vd_game_" + str(index)]
 
-        print("Set game " + str(index))
-
-        teams = standings["league"]["standard"]["teams"]
-
         for team in teams:
             if str(match["hTeam"]["teamId"]) == str(team["teamId"]):
-
-                print("Team found : " + str(team["homeWin"] + "-" + team["homeLoss"]) + " " + str(teams.index(team) + 1))
-                
                 vd_domicile_textview = vd_domicile.TextItem
                 vd_domicile_textview.contents = str(team["homeWin"] + "-" + team["homeLoss"])
-
                 rank_domicile_textview = rank_domicile.TextItem
                 rank_domicile_textview.contents = str(teams.index(team) + 1)
                 
 
             if match["vTeam"]["teamId"] == team["teamId"]:
-               
-                print("Team found : " + str(team["homeWin"] + "-" + team["homeLoss"]) + " " + str(teams.index(team) + 1))
                 vd_exterieur_textview = vd_exterieur.TextItem
                 vd_exterieur_textview.contents = str(team["awayWin"] + "-" + team["awayLoss"])
-
                 rank_exterieur_textview = rank_exterieur.TextItem
                 rank_exterieur_textview.contents = str(teams.index(team) + 1)
-
-
-
         
-    
+        print("Modification OK ! Game " + str(index) + " is " + match["hTeam"]["triCode"] + " vs " + match["vTeam"]["triCode"])
+
+    print("Fin du programme")
 
 
 def getPhotoshopFile(teamsNumber):
@@ -166,10 +149,19 @@ def getPhotoshopFile(teamsNumber):
 
 
 
-                
-                
+def parseMode(mode):
+    print("Selected mode : " + mode)
+    if mode == "OG":
+        getOtherGames()
+    if mode == "AG":
+        getAllGames()                       
 
-
+def getTeamStats(teamId):
+    for team in standings["league"]["standard"]["teams"]:
+        if(team["teamId"] == teamId):
+            print(team["confRank"])
+            print(team["homeWin"] + "-" + team["homeLoss"])
+            print(team["awayWin"] + "-" + team["awayLoss"])
     
 def getGameOfTheDay(gameId):
     global scoreboard
